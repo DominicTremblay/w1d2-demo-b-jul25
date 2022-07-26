6aa52a6357628788069a36ad6b5381f9630006df
Dominic Tremblay <dominictremblay24@gmail.com>
2022-07-26 15:07:57 -0400

    refacotring with helper functions

--- sum.js
+++ sum.js
@@ -8,47 +8,65 @@ const args = process.argv.slice(2);
 // Edge case: We need at least 2 arguments
 
 if (args.length < 2) {
-  console.log("Please enter at least 2 arguments");
+  console.log('Please enter at least 2 arguments');
   process.exit();
 }
 
-
 // check if the arguments are valid?
 // What are valid args? => // console.log args
-console.log("args:", args);
+console.log('args:', args);
 
-
-// iterate through the args
-// for of
-// c-style loop (let i=0; i < args.length; i++)
-
-// accumulator
-let total = 0;
-
-for (let nb of args) {
-  // add them up => calculate the sum
-  // Edge cases: If any argument is not a whole number, skip it.
-  // Math.floor
-  // float()?
-  // modulo (%) nb % 1 === 0
-  // Number.isInteger
-  const convertedNum = Number(nb);
-  // Edge cases: If any argument is not a number, output an error message.
-  // typeof
-
-  if (isNaN(convertedNum)) {
-    console.log("Please enter only numbers");
-    process.exit()
+const convertedNum = function(args) {
+  const result = [];
+  for (let nb of args) {
+    result.push(Number(nb))
   }
+  return result;
 
-  if(Number.isInteger(convertedNum)) {
-    total += convertedNum;
+}
+
+const onlyNumbers = function(numbers) {
+  for (let nb of numbers) {
+    if (isNaN(nb)) {
+      console.log('Please enter only numbers');
+      process.exit();
+    }
   }
-  console.log("nb:", nb, "convertedNum",convertedNum, "total:", total);
+  return numbers;
 }
 
 
 
+const sum = function (numbers) {
+  // iterate through the args
+  // for of
+  // c-style loop (let i=0; i < args.length; i++)
+
+  // accumulator
+  let total = 0;
+
+  for (let nb of numbers) {
+    // add them up => calculate the sum
+    // Edge cases: If any argument is not a whole number, skip it.
+    // Math.floor
+    // float()?
+    // modulo (%) nb % 1 === 0
+    // Number.isInteger
+    // Edge cases: If any argument is not a number, output an error message.
+    // typeof
+
+
+
+    if (Number.isInteger(nb)) {
+      total += nb;
+    }
+    // console.log('nb:', nb, 'convertedNum', convertedNum, 'total:', total);
+  }
+  return total;
+};
+
+
+const result = sum(onlyNumbers(convertedNum(args)));
 
 // print the sum
-console.log("Sum:", total);
+console.log('Sum:', result);
